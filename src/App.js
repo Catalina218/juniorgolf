da6a4982cb606075df0af3a1b0e57b89ca36e052
Fankasy <fankasy@hotmail.com>
2020-09-09 16:35:13 -0700

    add breadcrumbs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import NavBreadcrumb from './components/NavBreadcrumb/NavBreadcrumb'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <NavBreadcrumb/>
         <BrowserRouter>
           
         </BrowserRouter>
