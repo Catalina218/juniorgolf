838346f49032d481f3c7d7b517bac6a426876581
RuiGan <rgan1@uci.edu>
2020-09-23 21:51:14 -0700

    fix photo position

--- src/pages/Clinics/Clinics.jsx
+++ src/pages/Clinics/Clinics.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Card, Container, Image, Spinner } from 'react-bootstrap';
 import clinicsText from '../../assets/ClinicsText';
 import '../../styles/Clinics.css';
+import { storage } from '../../firebase';
 
 const Clinics = () => {
+  const [imageUrls, setImageUrls] = useState([]);
+  const storageRef = storage.ref();
+
+  const getImageUrl = async () => {
+    const imageRefs = await storageRef.child('clinics').listAll();
+    const imageNames = imageRefs.items.map(item => item.location.path);
+    const urlPromises = imageNames.map(name => {
+      return storageRef.child(name).getDownloadURL();
+    });
+    Promise.all(urlPromises).then(values => {
+      setImageUrls(values);
+    });
+  }
+
+  useEffect(() => {
+    getImageUrl();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="clinic">
       <Container>
@@ -25,6 +45,28 @@ const Clinics = () => {
                if you have any questions or concerns!
             </Card.Text>
           </Card.Body>
+          {
+          imageUrls.length > 0 ?
+            <div>
+            {
+              imageUrls.map((imageUrl, index) => {
+                return (
+                  <Image
+                    key={index}
+                    src={imageUrl}
+                    width="100%"
+                    className="clinics-image" 
+                  /> 
+                )
+              })
+            }
+            </div> :
+            <div className="clinics-spinner">
+              <Spinner animation="border" role="status" >
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            </div>
+          }
         </Card>
       </Container>
     </div>
